6a31e55567b6be329f5d8e2452b6425aa24507e2
Rafael Acevedo <rafael.acevedo@inlocomedia.com>
2021-06-15 09:27:19 -0300

    feat(TokenAwareNetworkingClient): changes to fully support oauth client credentials
    Read expiration from response json instead of decoding token
    Use token type from response json instead of hardcoding 'Bearer'
    Send grant_type=client_credentials in body for token requests

--- src/main/java/com/incognia/TokenAwareNetworkingClient.java
+++ src/main/java/com/incognia/TokenAwareNetworkingClient.java
@@ -1,12 +1,9 @@
 package com.incognia;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import okhttp3.OkHttpClient;
 
@@ -14,11 +11,13 @@ public class TokenAwareNetworkingClient {
   private static final String TOKEN_PATH = "api/v1/token";
   private static final String AUTHORIZATION_HEADER = "Authorization";
   private static final int TOKEN_REFRESH_BEFORE_SECONDS = 10;
+  private static final String TOKEN_REQUEST_BODY = "grant_type=client_credentials";
 
   private final NetworkingClient networkingClient;
   private final String clientId;
   private final String clientSecret;
-  private DecodedJWT token;
+  private TokenResponse token;
+  private Instant tokenExpiration;
 
   public TokenAwareNetworkingClient(
       OkHttpClient httpClient, String baseUrl, String clientId, String clientSecret) {
@@ -26,6 +25,7 @@ public class TokenAwareNetworkingClient {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
     this.token = null;
+    this.tokenExpiration = null;
   }
 
   public <T, U> U doPost(String path, T body, Class<U> responseType) throws IncogniaException {
@@ -34,13 +34,13 @@ public class TokenAwareNetworkingClient {
         path,
         body,
         responseType,
-        Collections.singletonMap(AUTHORIZATION_HEADER, "Bearer " + token.getToken()));
+        Collections.singletonMap(AUTHORIZATION_HEADER, buildAuthorizationHeader()));
   }
 
   public <T> void doPost(String path, T body) throws IncogniaException {
     refreshTokenIfNeeded();
     networkingClient.doPost(
-        path, body, Collections.singletonMap(AUTHORIZATION_HEADER, "Bearer " + token.getToken()));
+        path, body, Collections.singletonMap(AUTHORIZATION_HEADER, buildAuthorizationHeader()));
   }
 
   public <T> T doGet(String path, Class<T> responseType) throws IncogniaException {
@@ -48,26 +48,30 @@ public class TokenAwareNetworkingClient {
     return networkingClient.doGet(
         path,
         responseType,
-        Collections.singletonMap(AUTHORIZATION_HEADER, "Bearer " + token.getToken()));
+        Collections.singletonMap(AUTHORIZATION_HEADER, buildAuthorizationHeader()));
+  }
+
+  private String buildAuthorizationHeader() {
+    return token.getTokenType() + " " + token.getAccessToken();
   }
 
   private void refreshTokenIfNeeded() throws IncogniaException {
     if (token == null
-        || Instant.now().until(token.getExpiresAt().toInstant(), ChronoUnit.SECONDS)
+        || Instant.now().until(tokenExpiration, ChronoUnit.SECONDS)
             <= TOKEN_REFRESH_BEFORE_SECONDS) {
       // TODO(rato): handle concurrent requests
       token = getNewToken();
+      tokenExpiration = Instant.now().plusSeconds(token.getExpiresIn());
     }
   }
 
-  private DecodedJWT getNewToken() throws IncogniaException {
-    Map<String, String> headers = new HashMap<>();
-    headers.put("Content-Type", "application/x-www-form-urlencoded");
+  private TokenResponse getNewToken() throws IncogniaException {
     String clientIdSecret = clientId + ":" + clientSecret;
-    headers.put(
-        AUTHORIZATION_HEADER,
-        "Basic " + Base64.getUrlEncoder().encodeToString(clientIdSecret.getBytes()));
-    TokenResponse tokenResponse = networkingClient.doPost(TOKEN_PATH, TokenResponse.class, headers);
-    return JWT.decode(tokenResponse.getAccessToken());
+    Map<String, String> headers =
+        Collections.singletonMap(
+            AUTHORIZATION_HEADER,
+            "Basic " + Base64.getUrlEncoder().encodeToString(clientIdSecret.getBytes()));
+    return networkingClient.doPostFormUrlEncoded(
+        TOKEN_PATH, TOKEN_REQUEST_BODY, TokenResponse.class, headers);
   }
 }
