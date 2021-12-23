dc6b1f2ba19264b7ff15aaa8159b6c961df445a3
Amaro <joao.assuncao@incognia.com>
2021-12-23 14:11:06 -0300

    refactor(ObjectMapperFactory): makes the static object public

--- src/main/java/com/incognia/clients/ObjectMapperFactory.java
+++ src/main/java/com/incognia/clients/ObjectMapperFactory.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 public class ObjectMapperFactory {
-  static final ObjectMapper OBJECT_MAPPER =
+  public static final ObjectMapper OBJECT_MAPPER =
       new ObjectMapper()
           .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
           .setSerializationInclusion(Include.NON_NULL);
