575f03b8216d4d6a23716d498f26990107e1823e
Gabriel Meireles <gabriel.meireles@incognia.com>
2022-06-20 15:03:06 -0300

    Spotless apply

--- src/main/java/com/incognia/api/IncogniaAPI.java
+++ src/main/java/com/incognia/api/IncogniaAPI.java
@@ -21,7 +21,6 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import okhttp3.OkHttpClient;
