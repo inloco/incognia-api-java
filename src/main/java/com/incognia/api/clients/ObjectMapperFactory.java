bbfe3ec6b6186b7f34603f53e6af0914ca7c943a
Rafael Acevedo <rafael.acevedo@incognia.com>
2024-04-25 14:49:07 -0300

    Do not fail to deserialize response json when unknown fields are introduced (#215)

--- src/main/java/com/incognia/api/clients/ObjectMapperFactory.java
+++ src/main/java/com/incognia/api/clients/ObjectMapperFactory.java
@@ -1,6 +1,7 @@
 package com.incognia.api.clients;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
@@ -11,5 +12,6 @@ public class ObjectMapperFactory {
   public static final ObjectMapper OBJECT_MAPPER =
       new ObjectMapper()
           .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-          .setSerializationInclusion(Include.NON_NULL);
+          .setSerializationInclusion(Include.NON_NULL)
+          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
