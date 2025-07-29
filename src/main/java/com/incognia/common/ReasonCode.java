43fb367463aa8436e98b4a8396ac1d37a33e1ec5
Wallace Soares <wallace.soares@incognia.com>
2025-07-29 10:12:03 -0300

    Adding custom cancellation Feedback type (#273)

--- src/main/java/com/incognia/common/ReasonCode.java
+++ src/main/java/com/incognia/common/ReasonCode.java
@@ -56,7 +56,8 @@ public enum ReasonCode {
   DEVICE_LINKED_TO_CUSTOM_DISCIPLINE_BLOCK("device_linked_to_custom_discipline_block"),
   DEVICE_LINKED_TO_CUSTOM_COLLUSION_FRAUD("device_linked_to_custom_collusion_fraud"),
   DEVICE_LINKED_TO_CUSTOM_CARGO_FRAUD("device_linked_to_custom_cargo_fraud"),
-  DEVICE_LINKED_TO_CUSTOM_DEBT_CHURN_20D("device_linked_to_custom_debt_churn_20d");
+  DEVICE_LINKED_TO_CUSTOM_DEBT_CHURN_20D("device_linked_to_custom_debt_churn_20d"),
+  DEVICE_LINKED_TO_CUSTOM_CANCELLATION("device_linked_to_custom_cancellation");
 
   private final String code;
 
