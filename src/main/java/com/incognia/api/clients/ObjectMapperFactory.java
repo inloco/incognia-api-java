3435b6196451bd5d565c2da26f2022e6b20cf8bb
Wallace Soares <wallace.soares@incognia.com>
2026-02-19 17:09:32 -0300

    Upgrade dependencies and modify code accordingly

--- src/main/java/com/incognia/api/clients/ObjectMapperFactory.java
+++ src/main/java/com/incognia/api/clients/ObjectMapperFactory.java
@@ -3,15 +3,15 @@ package com.incognia.api.clients;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 public class ObjectMapperFactory {
   @SuppressWarnings("deprecation")
-  // PropertyNamingStrategy.SNAKE_CASE is deprecated but we use it for compatibility with older
+  // PropertyNamingStrategies.SNAKE_CASE is deprecated but we use it for compatibility with older
   // jackson versions
   public static final ObjectMapper OBJECT_MAPPER =
       new ObjectMapper()
-          .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+          .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
           .setSerializationInclusion(Include.NON_NULL)
           .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
